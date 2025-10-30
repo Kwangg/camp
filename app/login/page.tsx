5e5c1970dc4eb717150ae38d342f0d0ac48030e0
kwang <kwang.panitaa@gmail.com>
2025-10-30 09:30:15 +0700

    30/10

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,13 +8,13 @@ const LoginPage = () => {
   return (
     <div className="container mx-auto">
         <form action="">
-            <input className="block bg-gray-300 p-2 my-2 rounded-md" type="text" placeholder='Enter your name'/>
+            
             <input className="block bg-gray-300 p-2 my-2 rounded-md" type="email" placeholder='Enter your email'/>
             <input className="block bg-gray-300 p-2 my-2 rounded-md" type="password" placeholder='Enter your password'/>
             <Button type="submit" className="bg-green-500 p-2 rounded-md text-white">Sign in</Button>
         </form>
         <hr className="my-3"/>
-        <p>Already have an account? go to  <Link className="" href="/register">Register</Link> Page</p>
+        <p>Do not have an account? go to  <Link className="text-blue-500 hover:underline" href="/register">Register</Link> Page</p>
     </div>
   )
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,17 +1,71 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
-import React,{useState} from "react"
+import Link from "next/link"
+import React, { useState } from "react"
 
 const RegisterPage = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password != confirmPassword) {
+      setError("Password do not match")
+      return;
+    }
+
+    if (!name || !email || !password || !confirmPassword) {
+      setError("Please complete all input");
+      return;
+    }
+
+    try {
+      const res = await fetch("http://localhost:3000/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name, email, password
+        })
+      })
+      if (res.ok){
+  const form =e.target;
+  setError("");
+  form.reset();
+}else{
+  console.log("User registration failed")
+}
+    } catch (error) {
+      console.log("Error during registration:", error);
+    }
+  }
+
   return (
     <div className="container mx-auto">
-        <form action="">
-            <input className="block bg-gray-300 p-2 my-2 rounded-md" type="text" placeholder='Enter your name'/>
-            <input className="block bg-gray-300 p-2 my-2 rounded-md" type="email" placeholder='Enter your email'/>
-            <input className="block bg-gray-300 p-2 my-2 rounded-md" type="password" placeholder='Enter your password'/>
-            <Button type="submit" className="bg-green-500 p-2 rounded-md text-white">Sign up</Button>
-        </form>
+      <form onSubmit={handleSubmit}>
+
+        {error && (
+          <div className="bg-red-500 w-fit text-sm text-white py-1 px-3 rounded-md mt-2">
+            {error}
+          </div>
+        )}
+
+        <input onChange={(e) => setName(e.target.value)} className="block bg-gray-300 p-2 my-2 rounded-md" type="text" placeholder='Enter your name' />
+        <input onChange={(e) => setEmail(e.target.value)} className="block bg-gray-300 p-2 my-2 rounded-md" type="email" placeholder='Enter your email' />
+        <input onChange={(e) => setPassword(e.target.value)} className="block bg-gray-300 p-2 my-2 rounded-md" type="password" placeholder='Enter your password' />
+        <input onChange={(e) => setconfirmPassword(e.target.value)} className="block bg-gray-300 p-2 my-2 rounded-md" type="password" placeholder='Confirm your password' />
+        <Button type="submit" className="bg-green-500 p-2 rounded-md text-white">Sign up</Button>
+      </form>
+      <hr className="my-3" />
+      <p>Already have an account? go to  <Link className="text-blue-500 hover:underline" href="/login">Login</Link> Page</p>
+
     </div>
   )
 }
